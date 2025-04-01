003426a132a3d2beb6c8bf69fa801fa157c98eb3
RaulCamargoPenna <144546853+RaulCamargoPenna@users.noreply.github.com>
2025-04-01 20:42:26 -0300

    1

--- teste/tests.py
+++ teste/tests.py
@@ -3,9 +3,9 @@ from teste.views import soma
 # Create your tests here.
 
 def teste_soma():
-    resultado_esperado = 5
+    resultado_esperado = 6
     
     
-    resultado = soma(3, 2)
+    resultado = soma(3, 3)
     
     assert resultado_esperado == resultado
