35405194b6b26d82c13267a177cb84b1ee1e9dba
Raul Camargo <144546853+RaulCamargoPenna@users.noreply.github.com>
2025-04-02 15:27:23 -0300

    push na branch raul teste de buil sucess gerar pr auto

--- compras/tests.py
+++ compras/tests.py
@@ -14,7 +14,7 @@ from compras.views import comprar
 class SampleTestCase(TestCase):
     def test_example(self):
 
-        valor_produto = 14
+        valor_produto = 9
 
         resultado_esperado = True
 
