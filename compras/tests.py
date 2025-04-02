3937537a1437011fc5f7e429ec8df37eabbd6bbe
Raul Camargo <144546853+RaulCamargoPenna@users.noreply.github.com>
2025-04-02 15:06:42 -0300

    Teste failed para ver se pr auto vai ser executada

--- compras/tests.py
+++ compras/tests.py
@@ -1,20 +1,20 @@
 from django.test import TestCase
 from compras.views import comprar
 
-def teste_comprar():
+# def teste_comprar():
 
-    valor_produto = 9
+#     valor_produto = 9
 
-    resultado_esperado = True
+#     resultado_esperado = True
 
-    resultado = comprar(valor=valor_produto)
+#     resultado = comprar(valor=valor_produto)
 
-    assert resultado == resultado_esperado
+#     assert resultado == resultado_esperado
 
 class SampleTestCase(TestCase):
     def test_example(self):
 
-        valor_produto = 9
+        valor_produto = 11
 
         resultado_esperado = True
 
--- compras/views.py
+++ compras/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def comprar(valor):
 
-    if valor < 10:
+    if valor <= 10:
         return True
     
     return False
